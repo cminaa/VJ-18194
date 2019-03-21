3eff9c481fd6f4264290766bf6d26db53e12087e
Esmina Radusic <eradusic1@etf.unsa.ba>
2019-03-21 21:14:35 +0100

    zapoceta klasa muzicar

---
+++ app/src/main/java/ba/unsa/etf/rma/VJ_18194/Muzicar.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rma.VJ_18194;
+
+public class Muzicar {
+
+    String imeIPrezime="", zanr="", webStranica="", biografija="";
+
+    public Muzicar(String imeIPrezime, String zanr, String webStranica, String biografija) {
+        this.imeIPrezime = imeIPrezime;
+        this.zanr = zanr;
+        this.webStranica = webStranica;
+        this.biografija = biografija;
+    }
+}
