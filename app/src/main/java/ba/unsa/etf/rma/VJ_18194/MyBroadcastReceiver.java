7a99edfdc2dcb22fb77dd13fba0091ce1d84d49c
Esmina Radusic <eradusic1@etf.unsa.ba>
2019-03-30 17:42:20 +0100

    samostalni 3.

---
+++ app/src/main/java/ba/unsa/etf/rma/VJ_18194/MyBroadcastReceiver.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rma.VJ_18194;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        if (!isConnected) {
+            Toast.makeText(context, "There is no internet connection" , Toast.LENGTH_LONG);
+        }
+    }
+
+}
